3b92f1ffe9b9f03a1ef6769a670e0ed8857924d8
abin <aruvinnwijaya@gmail.com>
2024-09-28 14:04:50 +0700

    connected firestore

--- app/src/main/java/com/example/lspdatabase/MainActivity.kt
+++ app/src/main/java/com/example/lspdatabase/MainActivity.kt
@@ -1,47 +1,30 @@
 package com.example.lspdatabase
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.lspdatabase.ui.theme.LSPdatabaseTheme
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            LSPdatabaseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_main)
+
+        val db = FirebaseFirestore.getInstance()
+
+        val product1 = hashMapOf(
+            "name" to "Ayam Goreng",
+            "price" to 15000,
+            "stock" to 10,
+            "image_url" to "https://cdn1-production-images-kly.akamaized.net/zd-q4S3lk95fdXB0bDQ7EijRZYw=/1360x766/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/2958639/original/057463300_1572940011-shutterstock_127677473.jpg"
+        )
+
+        db.collection("products").add(product1)
+            .addOnSuccessListener { documentReference ->
+                Log.d("Firestore", "DocumentSnapshot added with ID: ${documentReference.id}")
             }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LSPdatabaseTheme {
-        Greeting("Android")
+            .addOnFailureListener { e ->
+                Log.w("Firestore", "Error adding document", e)
+            }
     }
 }
